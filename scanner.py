8df7e8910736a5b5abef48f0311d2cfee3e1ce56
WaIPeX <szymon.normalnie@gmail.com>
2024-09-01 04:24:33 +0200

    V2
    change host ip to use

--- scanner.py
+++ scanner.py
@@ -1,116 +1,109 @@
-import socket
+import ipaddress
 import os
+import socket
 import struct
+import sys
 import threading
-from ipaddress import ip_address, ip_network
-import ctypes
+import time
 
-#host to listen on
-host = "192.168.1.217" #Example
-
-#subnet to target
-tgt_subnet = "192.168.0.0/24" #Example
-
-
-tgt_message = "PYTHONRULES!" #Check message
-
-def udp_sender(sub_net, msg):
-    sender = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-    for ip in ip_network(sub_net).hosts():
-        sender.sendto(msg.encode("utf-8"), (str(ip), 65212))
-
-class IP(ctypes.Structure):
-    _fields_= [
-        ("ihl",          ctypes.c_ubyte, 4),
-        ("version",      ctypes.c_ubyte, 4),
-        ("tos",          ctypes.c_ubyte),
-        ("len",          ctypes.c_ushort),
-        ("id",           ctypes.c_ushort),
-        ("offset",       ctypes.c_ushort),
-        ("ttl",          ctypes.c_ubyte),
-        ("protocol_num", ctypes.c_ubyte),
-        ("sum",          ctypes.c_ushort),
-        ("src",          ctypes.c_uint32),
-        ("dst",          ctypes.c_uint32)
-    ]
-#IP Classes, internet research based xD
-    def __new__(cls, socket_buffer=None):
-        return cls.from_buffer_copy(socket_buffer)
+SUBNET = '192.168.1.0/24' # Example
+msg = 'Example'
+#I love stackoverflow
+class IP:
+    def __init__(self, buff=None):
+        header = struct.unpack('<BBHHHBBH4s4s', buff)
+        self.ver = header[0] >> 4
+        self.ihl = header[0] & 0xF
     
-    def __init__(self, socket_buffer=None):
-        self.socket_buffer = socket_buffer
+        self.tos = header[1]
+        self.len = header[2]
+        self.id = header[3]
+        self.offset = header[4]
+        self.ttl = header[5]
+        self.protocol_num = header[6]
+        self.sum = header[7]
+        self.src = header[8]
+        self.dst = header[9]
 
+        self.src_address = ipaddress.ip_address(self.src)
+        self.dst_address = ipaddress.ip_address(self.dst)
 
-        self.protocol_map = {1: "ICMP", 6: "TCP", 17:"UDP"} #mpa names
-
-        
-        self.src_address = socket.inet_ntoa(struct.pack("@I", self.src))
-        self.dst_address = socket.inet_ntoa(struct.pack("@I", self.dst)) # ip
-
-      
+        # map 
+        self.protocol_map = {1: "ICMP", 6: "TCP", 17: "UDP"}
         try:
-            self.protocol = self.protocol_map[self.protocol_num]   # protocl
-        except IndexError:
+            self.protocol = self.protocol_map[self.protocol_num]
+        except Exception as e:
+            print('%s No protocol for %s' % (e, self.protocol_num))
             self.protocol = str(self.protocol_num)
 
-class ICMP(ctypes.Structure): #icmp struct
-    _fields_= [
-        ("type",         ctypes.c_ubyte),
-        ("code",         ctypes.c_ubyte),
-        ("checksum",     ctypes.c_ushort),
-        ("unused",       ctypes.c_ushort),
-        ("next_hop_mtu", ctypes.c_ushort)
-    ]
-
-    def __new__(cls, socket_buffer):
-        return cls.from_buffer_copy(socket_buffer)
-
-    def __init__(self, socket_buffer):
-        self.socket_buffer = socket_buffer
+class ICMP:
+    def __init__(self, buff):
+        header = struct.unpack('<BBHHH', buff)
+        self.type = header[0]
+        self.code = header[1]
+        self.sum = header[2]
+        self.id = header[3]
+        self.seq = header[4]
 
 
-if os.name == "nt":
-    socket_protocol = socket.IPPROTO_IP #win sock
-else:
-    socket_protocol = socket.IPPROTO_ICMP #unx sock
-sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket_protocol)
+def udp_sender():
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sender:
+        for ip in ipaddress.ip_network(SUBNET).hosts():
+            time.sleep(1)
+            print('+', end='')
+            sender.sendto(bytes(msg, 'utf8'), (str(ip), 65212))
+            
 
-sniffer.bind(("0.0.0.0", 6677)) # Example, could be ...(host, port)
+class Scanner:
+    def __init__(self, host):
+        self.host = host
+        if os.name == 'nt':
+            socket_protocol = socket.IPPROTO_IP
+        else:
+            socket_protocol = socket.IPPROTO_ICMP
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket_protocol)
+        self.socket.bind((host, 0))
+        self.socket.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+        print('hitting promiscuous mode...')
+        if  os.name == 'nt':
+            self.socket.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
 
-sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+    def sniff(self):
+        hosts_up = set([f'{str(self.host)} *'])
+        try:
+            while True:
+                print('.',end='')
+                raw_buffer = self.socket.recvfrom(65535)[0]
+                ip_header = IP(raw_buffer[0:20])
+                if ip_header.protocol == "ICMP":
+                    offset = ip_header.ihl * 4
+                    buf = raw_buffer[offset:offset + 8]
+                    icmp_header = ICMP(buf)
+                    
+                    if icmp_header.code == 3 and icmp_header.type == 3:
+                        if ipaddress.ip_address(ip_header.src_address) in ipaddress.IPv4Network(SUBNET):
+                            if raw_buffer[len(raw_buffer) - len(msg): ] == bytes(msg, 'utf8'):
+                                hosts_up.add(str(ip_header.src_address))
+                                print(f'Host Up: {str(ip_header.src_address)}')
+        except KeyboardInterrupt: # interrupt 
+            if  os.name == 'nt':
+                self.socket.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
+        
+            print('\nUser interrupted.')
+            if hosts_up:
+                print(f'\n\nSummary: Hosts up on {SUBNET}')
+            for host in sorted(hosts_up):
+                print(f'{host}')
+            print('')
+            sys.exit()
 
-# if we're on Windows we need to send IOCTL to send promiscuos mode
-if os.name == "nt":
-    sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
-
-# start sending packets
-t = threading.Thread(target=udp_sender, args=(tgt_subnet, tgt_message))
-t.start()
-
-try:
-    while True:
-        raw_buffer = sniffer.recvfrom(65535)[0] # single b
-
-        ip_header = IP(raw_buffer[:20]) 
-
-        print(f"Protocol: {ip_header.protocol}, {ip_header.src_address} -> {ip_header.dst_address}")
-
-        if ip_header.protocol == "ICMP": # self expl, look for firt if icmmp
-            offset = ip_header.ihl * 4 
-            buf = raw_buffer[offset:offset + ctypes.sizeof(ICMP)]
-
-            icmp_header = ICMP(buf)
-
-            print(f"ICMP -> Type: {icmp_header.type}, {icmp_header.code}")
-
-
-            if icmp_header.code == 3 and icmp_header.type == 3: # Check Code & Type 3 host ok no port
-
-                if ip_address(ip_header.src_address) in ip_network(tgt_subnet):
-                    if raw_buffer[len(raw_buffer)- len(tgt_message):] == tgt_message:
-                        print(f"Host Up: {ip_header.src_address}")
-
-except KeyboardInterrupt:
-    if os.name == "nt":
-        sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        host = sys.argv[1]
+    else:
+        host = '192.168.1.203' # Example, change to work, chjeck ur local ip or get win10049 error :(
+    s = Scanner(host)
+    time.sleep(10)
+    t = threading.Thread(target=udp_sender)
+    t.start()
+    s.sniff()
